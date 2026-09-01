07a2ac7638b67bf4b9774b9f7142b13ab3c482ad
omar-ma66 <devcodefree66@gmail.com>
2026-09-01 16:43:23 +0200

    acces a une API meteo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import Header from './Header/Header.jsx';
-import Weather from './Weather/Weather.jsx';
-import './App.css'; // Import des styles CSS classiques
+import React from "react";
+import Header from "./Header/Header.jsx";
+import Weather from "./Weather/Weather.jsx";
+import "./App.css"; // Import des styles CSS classiques
 
 export default function App() {
+  async function getWeatherData() {
+
+    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+    const url =
+      `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=Lyon&days=5&aqi=no&alerts=no`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.current.is_day);
+  }
+
+  getWeatherData();
   return (
     <div className="App">
       <Header />
